accc76215f5d8fd3e436d4a979fce4a24a9397bb
Alex Sampley <alex.sampley@gmail.com>
2026-06-16 12:35:08 -0600

    Reorg keys into their own files

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -11,18 +11,26 @@
       config =
         let
           cfg = config.my.wireguard;
-          addressMap = builtins.mapAttrs (n: v: v // { address = [ "192.168.4.${toString v.index}" ]; }) {
-            "miranda" = {
-              index = 2;
-              publicKey = "nRSWAlds+OpZM0t2QJuMD3jXc4reQUICorxy/PGFnww=";
-            };
-            "willheim" = rec {
-              index = 1;
-              listenPort = 55820;
-              endpoint = "asampley.ca:${toString listenPort}";
-              publicKey = "fp6yv+ur1tO/J76yIE9HcU0CMUpOa7WNO6jNIs8MUiM=";
-            };
-          };
+          addressMap =
+            builtins.mapAttrs
+              (
+                n: v:
+                v
+                // {
+                  address = [ "192.168.4.${toString v.index}" ];
+                  publicKey = lib.trim (builtins.readFile ../hosts/${n}/wireguard.pub);
+                }
+              )
+              {
+                "miranda" = {
+                  index = 2;
+                };
+                "willheim" = rec {
+                  index = 1;
+                  listenPort = 55820;
+                  endpoint = "asampley.ca:${toString listenPort}";
+                };
+              };
           local = addressMap.${config.networking.hostName};
           others = lib.filterAttrs (name: _: name != config.networking.hostName) addressMap;
         in
@@ -31,40 +39,36 @@
             wireguard-tools
           ];
 
-          networking.wg-quick.interfaces =
-            let
-            in
-            {
-              wg0 = {
-                address = map (a: "${a}/24") local.address;
-                listenPort = if local ? "listenPort" then local.listenPort else null;
-                privateKeyFile = "/etc/wireguard/privatekey";
+          networking.wg-quick.interfaces = {
+            wg0 = {
+              address = map (a: "${a}/24") local.address;
+              listenPort = local.listenPort or null;
+              privateKeyFile = "/etc/wireguard/privatekey";
 
-                peers = map (host: {
-                  endpoint = if host ? "endpoint" then host.endpoint else null;
-                  publicKey = host.publicKey;
-                  presharedKeyFile = "/etc/wireguard/presharedkey";
-                  allowedIPs = map (a: "${a}/32") host.address;
-                }) (builtins.attrValues others);
-              };
+              peers = map (host: {
+                endpoint = host.endpoint or null;
+                publicKey = host.publicKey;
+                presharedKeyFile = "/etc/wireguard/presharedkey";
+                allowedIPs = map (a: "${a}/32") host.address;
+              }) (builtins.attrValues others);
             };
+          };
 
-            networking.firewall.allowedUDPPorts = lib.mkIf cfg.openFirewall [ local.listenPort ];
+          networking.firewall.allowedUDPPorts = lib.mkIf cfg.openFirewall [ local.listenPort ];
 
-            networking.hosts = builtins.zipAttrsWith
-              (_: values: values)
-              (builtins.attrValues
-                (builtins.mapAttrs
-                  (name: v:
-                    builtins.listToAttrs
-                    (map
-                      (address: { name = address; value = "wg.${name}.local"; })
-                      v.address
-                    )
-                  )
-                  others
+          networking.hosts = builtins.zipAttrsWith (_: values: values) (
+            builtins.attrValues (
+              builtins.mapAttrs (
+                name: v:
+                builtins.listToAttrs (
+                  map (address: {
+                    name = address;
+                    value = "wg.${name}.local";
+                  }) v.address
                 )
-              );
+              ) others
+            )
+          );
         };
     };
 }
