feb43f22476279b0edac9bc61e176b739223d1d6
Alex Sampley <alex.sampley@gmail.com>
2026-09-08 17:07:41 -0600

    Fix neovim tree sitter script length

--- packages/neovim-tree-sitter.nix
+++ packages/neovim-tree-sitter.nix
@@ -11,43 +11,38 @@ stdenv.mkDerivation {
 
   phases = [ "installPhase" ];
 
-  installPhase = lib.strings.concatStringsSep "\n" (
-    [
-      ''
-        set -x
-        mkdir -p $out/parser
-      ''
-    ]
-    ++ (map
-      (lang: ''
-        GRAMMAR="${tree-sitter-grammars."tree-sitter-${lang}"}"
-        ln -s "$GRAMMAR/parser" "$out/parser/${lang}.so"
+  installPhase =
+    let
+      prefix = "tree-sitter-";
+      prefixLength = builtins.stringLength prefix;
+      langs = map (name: builtins.substring prefixLength (builtins.stringLength name) name) (
+        builtins.filter (name: (builtins.substring 0 prefixLength name) == prefix) (
+          builtins.attrNames tree-sitter-grammars
+        )
+      );
+      langPaths = map (lang: "${tree-sitter-grammars."tree-sitter-${lang}"}") langs;
+      count = builtins.length langs;
+    in
+    ''
+      mkdir -p $out/parser
+      GRAMMARS=(${lib.strings.concatStringsSep " " langPaths})
+      LANGS=(${lib.strings.concatStringsSep " " langs})
+
+      for i in $(seq 0 ${toString (count - 1)}); do
+        GRAMMAR=''${GRAMMARS[i]}
+        LANG=''${LANGS[i]}
+        ln -s "$GRAMMAR/parser" "$out/parser/$LANG.so"
         QUERIES="$GRAMMAR/queries"
 
         if [ -e "$QUERIES" ]; then
           find "$QUERIES" -type f -print0 | while read -d "" QUERY; do
             DIR=$(dirname "$QUERY")
-            OUT=queries/${lang}/''${QUERY##$QUERIES}
+            OUT=queries/$LANG/''${QUERY##$QUERIES}
             mkdir -p "$out/$(dirname $OUT)"
 
             substitute "$QUERY" "$out/$OUT" --replace-quiet '(#is-not? local)' ""
           done
         fi
-      '')
-      (
-        let
-          prefix = "tree-sitter-";
-          prefixLength = builtins.stringLength prefix;
-        in
-        #[
-        #  "rust"
-        #]
-        (map (name: builtins.substring prefixLength (builtins.stringLength name) name) (
-          builtins.filter (name: (builtins.substring 0 prefixLength name) == prefix) (
-            builtins.attrNames tree-sitter-grammars
-          )
-        ))
-      )
-    )
-  );
+      done
+    '';
 }
